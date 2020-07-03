c16fbbc56bdb8154fed574a238e693425ad3e87b
Justin Eldridge <eldridgejm@gmail.com>
2020-07-03 11:43:22 -0700

    Added homework 01.

---
+++ homeworks/01-python_basics/tests/q5_2.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 5_2',
+  'hidden': False,
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 1 <= universities_q2 <= 4
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
