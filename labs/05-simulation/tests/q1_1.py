3d9501656c44426ea2244287ec139b24c89c8319
Justin Eldridge <eldridgejm@gmail.com>
2020-07-31 13:29:42 -0700

    Added lab.

---
+++ labs/05-simulation/tests/q1_1.py
@@ -0,0 +1,23 @@
+test = {
+  'hidden':False,
+  'name': '1.1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 12 <= modified_result <= 31
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
