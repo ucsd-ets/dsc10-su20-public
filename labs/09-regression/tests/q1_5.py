8916362d976005f0b9800aa70f968b70ae3eb4b0
Justin Eldridge <eldridgejm@gmail.com>
2020-08-26 11:52:46 -0700

    Added lab.

---
+++ labs/09-regression/tests/q1_5.py
@@ -0,0 +1,23 @@
+test = {
+  'hidden': False,
+  'name': 'q1_5',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> correlation_guess == 1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
